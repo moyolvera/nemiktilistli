7d7f9394d9af31aaaa363314427f5258a39cc37c
Moy Olvera <moi6olvera@gmail.com>
2022-02-21 11:09:24 -0600

    Added preview component for web

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ import CheckBox from './CheckBox/CheckBox';
 import Container from './Container/Container';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
+import LinkSharingPreview from './LinkSharingPreview/LinkSharingPreview';
 import Locations from './Locations/Locations';
 import SplashScreen from './SplashScreen/SplashScreen';
 import Text from './Text/Text';
@@ -12,6 +13,7 @@ export {
   Container,
   Footer,
   Header,
+  LinkSharingPreview,
   Locations,
   SplashScreen,
   Text,

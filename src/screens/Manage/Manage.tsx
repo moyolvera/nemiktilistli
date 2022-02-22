2cd07386b85628ab7a42716333772b58eb1485d5
Moy Olvera <moi6olvera@gmail.com>
2022-02-21 23:47:05 -0600

    Added missing analytics log

--- src/screens/Manage/Manage.tsx
+++ src/screens/Manage/Manage.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 import { Container } from '@components';
+import { useLogScreen } from '@hooks';
 
 import styles from './Manage.styles';
 
 interface ManageProps {}
 
 function ManageScreen({}: ManageProps) {
+  useLogScreen({ screenName: 'Manage' });
+
   return (
     <Container>
       <View style={styles.wrapper}>
